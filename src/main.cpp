cf0032aa5b0b2c4e89e7f96e31ab7631cef98f12
jesus-salazar05 <j.salazarvidal@ugto.mx>
2021-03-25 23:28:40 -0600

    commit cambios

--- src/main.cpp
+++ src/main.cpp
@@ -36,8 +36,11 @@ void setup()
 
 void loop()
 {
+  /*
   func1(); //Button detection
   func2(); //Menu de seleccion
+  */
+ funcDesva(10);
 }
 /////////////////////////////////////////////
 void func1()
